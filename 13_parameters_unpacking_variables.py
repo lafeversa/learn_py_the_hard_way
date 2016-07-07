9df2f880a716774b49c2559cc069e4198095dd88
lafeversa <lafeversa@gmail.com>
2016-07-06 19:02:04 -0700

    adding exercise 13

---
+++ 13_parameters_unpacking_variables.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+
+script, first, second, third = argv
+
+script = raw_input()
+first = raw_input()
+second = raw_input()
+third = raw_input()
+
+print "The script is called:", script + script
+print "Your first variable is:", first + first
+print "Your second variable is:", second + second
+print "Your third variable is:", third + third
