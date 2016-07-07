97867298914fd8154917149c5cc47adbb8846b73
lafeversa <lafeversa@gmail.com>
2016-07-06 18:58:51 -0700

    adding exercise 5

---
+++ 05_more_variables_and_printing.py
@@ -0,0 +1,23 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+kilogram_vs_lbs = .453592
+kilo_conv = my_weight * kilogram_vs_lbs
+centi_vs_inch = 2.54
+centi_conv = my_height * centi_vs_inch
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "which is %d centimetres tall." % centi_conv
+print "He's %d pounds heavy." % my_weight
+print "Which is %d kilograms." % kilo_conv
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
